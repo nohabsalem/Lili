1a56c05f17a843acf2974dccb224d73fe9333a12
Noha <noha.ben-salem@laplateforme.io>
2025-03-26 13:29:40 +0100

    deletion of routes

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -4,8 +4,6 @@ import { bearerAuth } from 'hono/bearer-auth'
 import authRouter from './auth.router.js'
 import assoRouter from './asso.router.js'
 import restoRouter from './resto.router.js';
-import reservation from './reservation.router.js';
-// import CguRouter from './Cgu.router.js';
 import { verify } from 'hono/jwt'
 import authService from '../services/auth.service.js'
 import env from '../config/env.js'
@@ -22,12 +20,6 @@ app.get('/', (c) => c.text('Hello from Hono!'))
 app.route('/api', authRouter);
 app.route("/api/asso", assoRouter);
 app.route("/api/resto", restoRouter);
-app.route("/api/reservation", reservation);
-
-// console.log("Routes chargées :");
-// console.log(app.routes);
-
-// app.route("/api/Cgu", CguRouter);
 
 app.get(
   '/authenticated',
