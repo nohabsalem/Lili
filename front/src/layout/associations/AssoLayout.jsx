506014121028bdbbe3065b0e652c689c7c89bd39
Noha <noha.ben-salem@laplateforme.io>
2025-03-26 22:15:06 +0100

    removal of authguard and some comments

--- front/src/layout/associations/AssoLayout.jsx
+++ front/src/layout/associations/AssoLayout.jsx
@@ -4,16 +4,6 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Sidebar from "@/components/ui/sidebar-asso";
 import Header from "@/components/ui/navbar";
-import { ADMIN, ASSO, RESTO, MANAGER, AGENT } from "@/config";
-
-const stats = [
-  { id: 1, name: "Transactions every 24 hours", value: "44 million" },
-  { id: 2, name: "Assets under holding", value: "$119 trillion" },
-  { id: 3, name: "New users annually", value: "46,000" },
-  { id: 4, name: "Total Restaurants", value: "1,200" },
-  { id: 5, name: "Total Associations", value: "500" },
-  { id: 6, name: "Total Users", value: "150,000" },
-];
 
 export default function AssoLayout() {
   let navigate = useNavigate();
@@ -28,14 +18,6 @@ export default function AssoLayout() {
   let role = localStorage.getItem("role") ?? null;
   let navLinks = [];
 
-  // if (role === ASSO) {
-  //   navLinks = [
-  //     { name: "Dashboard", route: "/admin" },
-  //     { name: "Utilisateurs", route: "/users" },
-  //     { name: "Restaurants", route: "/restaurants" }
-  //   ]
-  // }
-
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     < div className="flex min-h-screen" >
@@ -46,7 +28,7 @@ export default function AssoLayout() {
       {/* Contenu principal */}
       <div className="flex flex-col flex-1">
         <Header toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
-      <h1>ASSO DASHBOARD</h1> 
+        <h1>ASSO DASHBOARD</h1>
         <main className="flex-1 p-6 w-full">
           <div className="mx-auto max-w-7xl w-full">
             <Outlet />
